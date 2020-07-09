429cfd9894f1eb2eca702323e7810f1d2b11fcf3
Farid <fa1rid@live.com>
2020-07-10 03:10:14 +0400

    Update script.sh

--- script.sh
+++ script.sh
@@ -3,8 +3,8 @@
 mkdir -p /root/wpa
 cd /root/wpa
 
-#wget "92.223.93.168/wpa03.p.7z"
-#wget "92.223.93.168/wpa02.p.7z"
-#wget "92.223.93.168/wpa01.p.7z"
+wget "92.223.93.168/wpa03.p.7z"
+wget "92.223.93.168/wpa02.p.7z"
+wget "92.223.93.168/wpa01.p.7z"
 wget "92.223.93.168/wpa00.p.7z"
 
