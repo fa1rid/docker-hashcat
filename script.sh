b9e197e6bf74b9c44322111f11b4222a64d7d144
Farid <fa1rid@live.com>
2020-07-10 02:32:53 +0400

    Create script.sh

---
+++ script.sh
@@ -0,0 +1,10 @@
+#!/bin/sh
+
+mkdir -p /root/wpa
+cd /root/wpa
+
+wget "92.223.93.168/wpa03.p.7z"
+wget "92.223.93.168/wpa02.p.7z"
+wget "92.223.93.168/wpa01.p.7z"
+wget "92.223.93.168/wpa00.p.7z"
+
