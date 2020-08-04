f205fcc613df1aaece6f8d3654c2dd4ad0be6b1d
Farid <fa1rid@live.com>
2020-08-05 03:47:17 +0400

    Update script.sh

--- script.sh
+++ script.sh
@@ -5,5 +5,5 @@ cd /root/wpa
 
 for i in {01..18}; do curl -JO "95.217.128.40:8000/100/w1.$i.7z"; done
 
-for i in {01..02}; do curl -JO "95.217.128.40:8000/100/w1_21+.$i.7z"; don
+for i in {01..02}; do curl -JO "95.217.128.40:8000/100/w1_21+.$i.7z"; done
 
